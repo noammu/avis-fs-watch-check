473585d8c15505d7a79542364acc1d05344892ca
Noam Musba <noammu@wix.com>
2024-06-10 17:48:50 +0300

    deleteee

--- src/_codux/boards/lkjlkj/lkjlkj.board.tsx
+++
@@ -1,8 +0,0 @@
-import { createBoard } from '@wixc3/react-board';
-import { Lkjlkj } from '../../../components/lkjlkj/lkjlkj';
-
-export default createBoard({
-    name: 'Lkjlkj',
-    Board: () => <Lkjlkj />,
-    isSnippet: true,
-});
--- src/_codux/boards/oneee/oneee.board.tsx
+++
@@ -1,8 +0,0 @@
-import { createBoard } from '@wixc3/react-board';
-import { Oneee } from '../../../components/oneee/oneee';
-
-export default createBoard({
-    name: 'Oneee',
-    Board: () => <Oneee />,
-    isSnippet: true,
-});
